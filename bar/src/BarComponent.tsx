024a0a72a3ed93a27a10e67b244917ae779dcf6d
Kristoffer Hansen <kristoffer.hansen@lego.com>
2023-05-10 16:22:41 +0200

    Remote with dynamic imports

--- bar/src/BarComponent.tsx
+++ bar/src/BarComponent.tsx
@@ -1,17 +1,20 @@
-import { useState } from "react";
-import { BarItem } from "./components/BarItem";
+import { useState, lazy, Suspense } from "react";
 
 export function BarComponent() {
 
   const [bars, setBars] = useState<string[]>([])
 
+  const BarItem = lazy(() => import('./components/BarItem'));
+
   return (
     <fieldset style={{backgroundColor: 'greenyellow'}}>
       <h2>This is bar!</h2>
       <button onClick={() => setBars((prev) => [...prev, "BAR"])}>Add bar!</button>
+      <Suspense fallback={<div>Loading...</div>}>
         <ul>
           {bars.map((bar, index) => <BarItem key={index}>{bar}</BarItem>)}
         </ul>
+      </Suspense>
     </fieldset>
   );
 }
