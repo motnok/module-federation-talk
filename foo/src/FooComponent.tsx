25c740b1015b6289a0cf16c13c65dceb233416c7
Kristoffer Hansen <kristoffer.hansen@lego.com>
2023-05-10 16:31:40 +0200

    Foo has an error :(

--- foo/src/FooComponent.tsx
+++ foo/src/FooComponent.tsx
@@ -1,10 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import isEven from 'is-even';
 
 export function FooComponent() {
 
   const [fooed, setFooed] = useState(0)
+
+  useEffect(() => {
+    if(fooed >= 10) 
+      throw new Error('Foo has died :(')
+  }, [fooed])
 
   return (
     <fieldset style={{backgroundColor: 'salmon'}}>
