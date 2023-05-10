5a79f88631b1c7759418035bc8b02ac35546ac6a
Kristoffer Hansen <kristoffer.hansen@lego.com>
2023-05-10 16:17:53 +0200

    Remotes with deps

--- foo/src/FooComponent.tsx
+++ foo/src/FooComponent.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+import isEven from 'is-even';
+
 export function FooComponent() {
 
   const [fooed, setFooed] = useState(0)
@@ -9,7 +11,7 @@ export function FooComponent() {
       <h2>This is foo!</h2>
       <button onClick={() => setFooed((prev) => prev+1)}>Foo me up!</button>
       <p>
-        Fooed up {fooed} times!
+        Fooed up {fooed} times! That's an {isEven(fooed) ? 'even' : 'uneven'} number btw.
       </p>
     </fieldset>
   );
